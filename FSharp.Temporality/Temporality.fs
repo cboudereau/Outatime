653e949e9bf26f3389a9f4b7118c9ed172c949df
cboudereau <cboudereau@gmail.com>
2015-05-09 01:51:49 +0200

    property based testing

--- FSharp.Temporality/Temporality.fs
+++ FSharp.Temporality/Temporality.fs
@@ -2,19 +2,40 @@
 
 type DateTime = System.DateTime
 
-type Period = 
-    { startDate: DateTime 
-      endDate: DateTime }
+type PeriodException(message) = 
+    inherit exn(message)
 
-type Bound = 
-    | Inclusive
-    | Exclusive
+type Period = 
+    { startDate : DateTime
+      endDate : DateTime }
 
 type BoundedPeriod = 
     | Inclusive of Period
     | Exclusive of Period
+    
+    static member ToInclusive(boundedPeriod) = 
+        let validPeriod = BoundedPeriod.PreCondition boundedPeriod
+        match validPeriod with
+        | Inclusive period -> period
+        | Exclusive period -> { period with endDate = period.endDate.AddDays(-1.) }
+    
+    static member ToExclusive(boundedPeriod) = 
+        let validPeriod = BoundedPeriod.PreCondition boundedPeriod
+        match validPeriod with
+        | Exclusive period -> period
+        | Inclusive period -> { period with endDate = period.endDate.AddDays(1.) }
+    
+    static member private PreCondition(boundedPeriod) = 
+        match boundedPeriod with
+        | Inclusive period -> 
+            if period.startDate >= period.endDate then 
+                raise (PeriodException(sprintf "%A start must be greater than %A" period.startDate period.endDate))
+            else boundedPeriod
+        | Exclusive period -> 
+            if period.startDate > period.endDate then 
+                raise (PeriodException(sprintf "%A start must be greater than %A" period.startDate period.endDate))
+            else boundedPeriod
 
-type Temporary<'a> =
-    { period: BoundedPeriod 
-      value: 'a }
-
+type Temporary<'a when 'a : equality and 'a : comparison> = 
+    { period : BoundedPeriod
+      value : 'a }
