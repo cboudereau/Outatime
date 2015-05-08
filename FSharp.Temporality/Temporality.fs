ca71340fd97ca3e9826248699459cef77e140646
cboudereau <cboudereau@gmail.com>
2015-05-08 21:50:36 +0200

    init

---
+++ FSharp.Temporality/Temporality.fs
@@ -0,0 +1,20 @@
+﻿module Temporality
+
+type DateTime = System.DateTime
+
+type Period = 
+    { startDate: DateTime 
+      endDate: DateTime }
+
+type Bound = 
+    | Inclusive
+    | Exclusive
+
+type BoundedPeriod = 
+    | Inclusive of Period
+    | Exclusive of Period
+
+type Temporary<'a> =
+    { period: BoundedPeriod 
+      value: 'a }
+
