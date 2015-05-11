357ace93870e28a66653556225c9f83db65858b5
cboudereau <cboudereau@gmail.com>
2015-05-11 09:02:51 +0200

    refactor fscheck for exceptions cases

--- FSharp.Temporality.Test/BoundedPeriodTest.fs
+++ FSharp.Temporality.Test/BoundedPeriodTest.fs
@@ -4,7 +4,7 @@ open Xunit
 open FsCheck
 open Temporality
 
-let ``Check for all valid period that:`` fs = 
+let ``Check for some valid periods that:`` fs = 
     let validPeriod = function 
     | Inclusive p -> p.startDate <= p.endDate 
     | Exclusive p -> p.startDate < p.endDate
@@ -17,26 +17,7 @@ let ``Check for all valid period that:`` fs =
     fs
     |> Seq.iter(fun f -> Check.QuickThrowOnFailure(Prop.forAll arb f))
 
-[<Fact>]
-let ``convert inclusive period to exclusive period``() = 
-    let ``start date never change`` boundedPeriod = 
-        (boundedPeriod |> BoundedPeriod.ToExclusive).startDate = (boundedPeriod |> BoundedPeriod.ToInclusive).startDate
-    
-    let ``exclusive end date must be greater than inclusive end date`` boundedPeriod =
-        (boundedPeriod |> BoundedPeriod.ToExclusive).endDate > (boundedPeriod |> BoundedPeriod.ToInclusive).endDate
-    
-    let ``same periods, same bound are equals`` boundedPeriod = 
-        match boundedPeriod with
-        | Inclusive ip -> boundedPeriod |> BoundedPeriod.ToInclusive = ip        
-        | Exclusive ep -> boundedPeriod |> BoundedPeriod.ToExclusive = ep        
-
-    ``Check for all valid period that:``
-        [ ``start date never change``
-          ``exclusive end date must be greater than inclusive end date``
-          ``same periods, same bound are equals`` ]
-
-[<Fact>]
-let ``startDate must be greater than endDate for BoundedPeriod``()=
+let ``Check for some invalid periods that throw``<'a when 'a :> exn> fs = 
     let invalidPeriod = function
     | Inclusive period -> period.startDate > period.endDate
     | Exclusive period -> period.startDate >= period.endDate
@@ -51,10 +32,32 @@ let ``startDate must be greater than endDate for BoundedPeriod``()=
             f boundedPeriod |> ignore
             false
         with
-        | :? BoundedPeriodException as ex -> true
+        | :? 'a as ex -> true
         | _ -> false
 
-    let ``inclusive case`` boundedPeriod = expectedBoundedPeriodException BoundedPeriod.ToInclusive  boundedPeriod
-    let ``exclusive case`` boundedPeriod = boundedPeriod |> BoundedPeriod.ToExclusive
+    fs
+    |> Seq.iter(fun f -> Check.QuickThrowOnFailure(Prop.forAll arb <| expectedBoundedPeriodException f))
+
+[<Fact>]
+let ``convert inclusive period to exclusive period``() = 
+    let ``start date never change`` boundedPeriod = 
+        (boundedPeriod |> BoundedPeriod.ToExclusive).startDate = (boundedPeriod |> BoundedPeriod.ToInclusive).startDate
+    
+    let ``exclusive end date must be greater than inclusive end date`` boundedPeriod =
+        (boundedPeriod |> BoundedPeriod.ToExclusive).endDate > (boundedPeriod |> BoundedPeriod.ToInclusive).endDate
+    
+    let ``same periods, same bound are equals`` boundedPeriod = 
+        match boundedPeriod with
+        | Inclusive ip -> boundedPeriod |> BoundedPeriod.ToInclusive = ip        
+        | Exclusive ep -> boundedPeriod |> BoundedPeriod.ToExclusive = ep        
+
+    ``Check for some valid periods that:``
+        [ ``start date never change``
+          ``exclusive end date must be greater than inclusive end date``
+          ``same periods, same bound are equals`` ]
         
-    Check.QuickThrowOnFailure(Prop.forAll arb ``inclusive case``)
+[<Fact>]
+let ``startDate must be greater than endDate for BoundedPeriod``()=
+    [ BoundedPeriod.ToInclusive 
+      BoundedPeriod.ToExclusive ]
+    |> ``Check for some invalid periods that throw``<BoundedPeriodException> 
