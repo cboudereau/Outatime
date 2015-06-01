8768d85d7f0bac64caa6b1e1fea403e0c1d48282
cboudereau <cboudereau@gmail.com>
2015-06-01 18:25:13 +0200

    clean

---
+++ FSharp.Temporality.Test/PeriodTest.fs
@@ -0,0 +1,32 @@
+﻿module PeriodTest
+
+open Temporality
+open FsUnit
+open Xunit
+
+let jan15 day = DateTime(2015, 01, day)
+
+[<Fact>]
+let ``TimeSpan composition test``()=
+    TimeSpan.forEver + TimeSpan.forNever |> should equal TimeSpan.forEver
+    TimeSpan.forEver - TimeSpan.forNever |> should equal TimeSpan.forEver
+    TimeSpan.forNever - TimeSpan.forNever |> should equal TimeSpan.forNever
+    TimeSpan.forNever + TimeSpan.forNever |> should equal TimeSpan.forNever
+
+    DateTime.MaxValue - DateTime.MinValue |> should equal TimeSpan.forEver
+
+[<Fact>]
+let ``Period should be display with math interval notation for half open interval``()=
+    let p = { StartDate = jan15 1; EndDate = jan15 2 }
+    
+    p.ToString() |> should equal "[1/1/2015 0:00:00, 2/1/2015 0:00:00)" 
+
+    Period.Always.ToString() |> should equal "Always"
+    Period.Never.ToString() |> should equal "Never"
+
+[<Fact>]
+let ``Temporary should be displayed same as Period``()=
+    let t = { Period = Period.Always; Value = "Hello" }
+
+    t.ToString() |> should equal @"Always : ""Hello"""
+
---
+++ FSharp.Temporality.Test/ViewProperties.fs
@@ -0,0 +1,18 @@
+﻿module ViewProperties
+
+open FsCheck.Xunit
+open Temporality
+
+[<Arbitrary(typeof<TestData.RandomViewTemporal>)>]
+module RangeProperty = 
+    [<Property>]
+    let ``StartDate of view should be always greater or equal the given viewed startDate period`` period 
+        (temporal : Temporal<string>) = 
+        let windowedTemporal = temporal |> Temporal.view period
+        windowedTemporal.Values |> Seq.forall (fun t -> t.Period.StartDate >= period.StartDate)
+    
+    [<Property>]
+    let ``EndDate of view should be always less or equal to the given viewed end date period`` () period 
+        (temporal : Temporal<string>) = 
+        let windowedTemporal = temporal |> Temporal.view period
+        windowedTemporal.Values |> Seq.forall (fun t -> t.Period.EndDate <= period.EndDate)
