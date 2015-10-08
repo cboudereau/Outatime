291d9519292fb6400e8c0cb53e60dff7eb23c8f2
cboudereau <cboudereau@gmail.com>
2015-10-08 08:42:55 +0200

    clean Bdd

--- FSharp.Temporality.Test/Bdd.fs
+++ FSharp.Temporality.Test/Bdd.fs
@@ -1,16 +1,5 @@
 ﻿module Bdd
-open Temporality
 open FsUnit.Xunit
-
-//let Given v = v
-//let When f v = v |> f
-//
-//let Then check expected = 
-//    check expected
-//
-//let shouldEqual expected actual = actual |> should equal expected
-//
-//let shouldBeEmpty actual = actual |> Seq.length |> should equal 0
 
 let When f = f
 let With v f = f v
